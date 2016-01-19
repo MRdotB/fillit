5afdd589a7f64e491ce69ad6ba93ff86ac1d38e2
mrdotb <baptiste.chaleil@gmail.com>
2016-01-19 17:42:38 +0100

    move get matrice to is own file

---
+++ srcs/get_matrice.c
@@ -0,0 +1,38 @@
+#include "lib.h"
+
+static char		*bt_concat(char c, char *str)
+{
+	char	*r;
+	int		i;
+
+	r = (char*)malloc(sizeof(char) * ft_strlen(str) + 2);
+	if (r == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		r[i] = str[i];
+		i++;
+	}
+	r[i++] = c;
+	r[i] = '\0';
+	free(str);
+	return (r);
+}
+
+char			*get_matrice(char *file_name)
+{
+	int fd;
+	char buf;
+	char *r;
+
+	r = (char*)malloc(sizeof(char) * 1);
+	if (r == NULL)
+		return (NULL);
+	r[0] = '\0';
+	fd = open(file_name, O_RDONLY);
+	while (read(fd, &buf, 1))
+		r = bt_concat(buf, r);
+	close(fd);
+	return (r);
+}
