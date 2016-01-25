07425ae4a5a91cdea3d303351a5c80c4f43b4ae6
mrdotb <baptiste.chaleil@gmail.com>
2016-01-25 22:25:13 +0100

    modify solve

--- srcs/solve.c
+++ srcs/solve.c
@@ -6,30 +6,11 @@
 /*   By: bchaleil <hello@baptistechaleil.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/22 11:54:37 by bchaleil          #+#    #+#             */
-/*   Updated: 2016/01/22 18:15:50 by bchaleil         ###   ########.fr       */
+/*   Updated: 2016/01/25 22:23:49 by bchaleil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lib.h"
-
-char	**fill_matrice(t_matrice matrice)
-{
-	int x;
-	int y;
-
-	y = 0;
-	while (y < matrice.size_y)
-	{
-		x = 0;
-		while (x < matrice.size_x)
-		{
-			matrice.map[y][x] = '.';
-			x++;
-		}
-		y++;
-	}
-	return (matrice.map);
-}
 
 static	t_pos	return_pos(int x, int y)
 {
@@ -38,6 +19,14 @@ static	t_pos	return_pos(int x, int y)
 	r.x = x;
 	r.y = y;
 	return (r);
+}
+
+static	t_pos	return_pos_modulo(t_pos pos, int len)
+{
+		pos.x = (pos.x + 1) % len;
+		if (pos.x == 0)
+			pos.y = (pos.y + 1) % len;
+		return (pos);
 }
 
 void	display_matrice(t_matrice matrice)
@@ -59,137 +48,44 @@ void	display_matrice(t_matrice matrice)
 	}
 }
 
-int		check_fill(int code, t_matrice matrice, t_pos pos)
+char	**recurse_solve(t_matrice matrice, t_tetrimino *tetri_list, t_pos pos)
 {
-	int	tmp;
-	int last;
-	int d;
+	char **tmp;
 
-	d = 1000;
-	last = 0;
-	tmp = 0;
-	while (d != 0)
+	if (tetri_list == NULL)
+		return (matrice.map);
+	if ((pos.x == matrice.size_x && pos.y == matrice.size_y))
+		return (NULL);
+	if (check_fill(tetri_list->signature, matrice, pos))
 	{
-		tmp = code / d;
-		code %= d;
-		d /= 10;
-		if (last == tmp && last != 0)
-			pos.y++;
-		else if (last > tmp && last != 0)
+		matrice.map = fill(tetri_list->signature, matrice, pos);
+		tetri_list->used = 1;
+		tmp = recurse_solve(matrice, tetri_list->next, return_pos(0, 0));
+		if (tmp == NULL)
 		{
-			pos.x += tmp - last;
-			pos.y++;
+			matrice.map = unfill(tetri_list->signature, matrice, pos);
+			tetri_list->used = 0;
+			return (recurse_solve(matrice, tetri_list, return_pos_modulo(pos, matrice.size_y)));
 		}
-		else if (last < tmp && last != 0)
-			pos.x -= last - tmp;
-		if (pos.x >= 0 && pos.y >= 0 && pos.x <= matrice.size_x && pos.y <= matrice.size_y)
-		{
-			if (matrice.map[pos.y][pos.x] == '#')
-				return (0);
-		}
-		else
-			return (0);
-		last = tmp;
+		return (tmp);
 	}
-	return (1);
-}
-
-char	**fill(int code, t_matrice matrice, t_pos pos)
-{
-	int	tmp;
-	int last;
-	int d;
-
-	d = 1000;
-	last = 0;
-	tmp = 0;
-	while (d != 0)
-	{
-		tmp = code / d;
-		code %= d;
-		d /= 10;
-		if (last == tmp && last != 0)
-			pos.y++;
-		else if (last > tmp && last != 0)
-		{
-			pos.x += tmp - last;
-			pos.y++;
-		}
-		else if (last < tmp && last != 0)
-			pos.x -= last - tmp;
-		matrice.map[pos.y][pos.x] = '#';
-		last = tmp;
-	}
-	return (matrice.map);
-}
-int		square_size(t_matrice matrice)
-{
-	int x;
-	int y;
-	int	r;
-
-	x = matrice.size_x;
-	y = matrice.size_y;
-	while (x >= 0 && y >= 0)
-	{
-		r = x;
-		while (x >= 0)
-		{
-			if (matrice.map[y][x] == '#')
-				return (r);
-			x--;
-		}
-		x = r - 1;
-		while (y >= 0)
-		{
-			if (matrice.map[y][x] == '#')
-				return (r);
-			y--;
-		}
-		y = r;
-		r = x;
-	}
-	return (0);
-}
-
-t_pos	find_next(int best, t_matrice matrice, int code)
-{
-	
-	if (check_fill(code, matrice, pos))
-	{
-		fill(code, matrice, pos);
-	}
-}
-
-
-int		recurse_solve(t_matrice	matrice, t_tetrimino **tetri_list, t_pos pos)
-{
-	t_tetrimino *list;
-
-	list = *tetri_list;
-	if (list == NULL)
-	{
-		display_matrice(matrice);
-		printf("%d\n", square_size(matrice));
-		return (1);
-	}
-	if (check_fill(list->signature, matrice, pos))
-	{
-		fill(list->signature, matrice, pos);
-		list->used = 1;
-		return (recurse_solve(matrice, &(list->next), return_pos(pos.x + 1, pos.y)));
-	}
-	else
-	{
-		recurse_solve(matrice, &list, return_pos(pos.x + 1, pos.y));
-	}
+	return (recurse_solve(matrice, tetri_list, return_pos_modulo(pos, matrice.size_y)));
 }
 
 void	solve(t_tetrimino *tetri_list)
 {
-	t_matrice		matrice;	
+	t_matrice		matrice;
+	int				len = 8;
 
-	matrice = create_matrice(10, 10);
-	matrice.map = fill_matrice(matrice);
-	recurse_solve(matrice, &tetri_list, return_pos(0, 0));
+	matrice = create_matrice(len, len);
+	matrice.map = recurse_solve(matrice, tetri_list, return_pos(0, 0));
+	display_matrice(matrice);
+/*
+	while ((matrice.map = recurse_solve(matrice, tetri_list, return_pos(0, 0))) == NULL)
+	{
+		
+	}
+	//if (matrice)
+		display_matrice(matrice);
+*/
 }
